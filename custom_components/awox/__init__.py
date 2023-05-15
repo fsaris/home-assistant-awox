3a74d79059fe8072ed9dd043696078e6cbaa8a60
Benjamin Aeby <58775333+b-aeby@users.noreply.github.com>
2023-05-15 20:56:12 +0200

    Update __init__.py
    Replace deprecated : hass.config_entries.async_setup_platforms(entry, PLATFORMS)

--- custom_components/awox/__init__.py
+++ custom_components/awox/__init__.py
@@ -35,7 +35,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     # Make `mesh` accessible for all platforms
     hass.data[DOMAIN][entry.entry_id] = mesh
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
 
     return True
 
